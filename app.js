809e25d40daa333e1c4a552bf8b251f29f7f67ba
potato-lulw <om.patil17072@gmail.com>
2024-01-04 02:12:44 +0530

    💾Fetching books by ID

--- app.js
+++ app.js
@@ -1,16 +1,66 @@
 const express = require('express');
+const {connectToDb, getDb} = require('./db');
+const { ObjectId } = require('mongodb');
 
-// init app & middlewares
+//~ init app & middlewares
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log('listening on port 3000');
+//~ db connection
+
+let db;
+connectToDb((err) => {
+    if(!err){
+        app.listen(3000, () => {
+            console.log('listening on port 3000');
+        });
+
+        db = getDb();
+    }
+    else{
+        console.log(err);
+    }
+})
+
+//~ routes
+
+app.get('/books', (req, res) => {
+    // res.json({mssg: "Welcome to the api"});
+
+    let books = [];
+
+    db.collection('books')
+        .find()
+        .sort({author: 1})
+        .forEach(book => books.push(book))
+        .then(() => {
+            res.status(200).send(books)
+        })
+        .catch(() => {
+            res.status(500).json({err: 'Could not fetch the books'}); 
+        })
 });
 
 
-// routes
+app.get('/books/:id', (req, res) => {
 
-app.get('/books', (req, res) => {
-    res.json({mssg: "Welcome to the api"});
+    const id = req.params.id;
+
+    if(ObjectId.isValid(id)){
+        db.collection('books')
+        .findOne({_id: new ObjectId(id)})
+        .then((book) => {
+            res.status(200).send(book);
+        })
+        .catch((err) => {
+            res.status(500).json({
+                error: "Book was not found, check book ID!",
+            });
+        })
+    }
+    else{
+        res.status(500).json({error:'Invalid book ID'})
+    }
+    
+
 });
